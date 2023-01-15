e6b60c2a2fb3b74476f30b0976f6d1334cd4e72f
Lars Buddenberg <mail@koyax.org>
2023-01-15 22:19:48 +0100

    Create entrypoint.sh (#12)
    * Create entrypoint.sh
    
    * Update Dockerfile
    
    * Update entrypoint.sh
    
    * Update Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sed -i 's|LAMA_API_URL = null|LAMA_API_URL = "'$LAMA_API_URL'"|g' /usr/share/nginx/html/config.js 
+sed -i 's|KEYCLOAK_URL = null|KEYCLOAK_URL = "'$KEYCLOAK_URL'"|g' /usr/share/nginx/html/config.js
+sed -i 's|KEYCLOAK_REALM = null|KEYCLOAK_REALM = "'$KEYCLOAK_REALM'"|g' /usr/share/nginx/html/config.js
+sed -i 's|KEYCLOAK_CLIENTID = null|KEYCLOAK_CLIENTID = "'$KEYCLOAK_CLIENTID'"|g' /usr/share/nginx/html/config.js
+
+nginx -g daemon off;
