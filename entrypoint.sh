9e349fa360af15f326c59ad9accb6e9792c1034e
Lars Buddenberg <mail@koyax.org>
2023-01-15 22:29:48 +0100

    Fix nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ sed -i 's|KEYCLOAK_URL = null|KEYCLOAK_URL = "'$KEYCLOAK_URL'"|g' /usr/share/ngi
 sed -i 's|KEYCLOAK_REALM = null|KEYCLOAK_REALM = "'$KEYCLOAK_REALM'"|g' /usr/share/nginx/html/config.js
 sed -i 's|KEYCLOAK_CLIENTID = null|KEYCLOAK_CLIENTID = "'$KEYCLOAK_CLIENTID'"|g' /usr/share/nginx/html/config.js
 
-nginx -g daemon off;
+nginx -g 'daemon off;'
