8097ebc66754f342092df27326989ce98ce7254a
Micha Preußer <mail@mpreusser.de>
2025-05-29 13:24:15 +0200

    Refactor GroupPage component to enhance user and participator editing functionality, improve group dialog handling, and update user data retrieval logic.

--- src/pages/GroupPage.vue
+++ src/pages/GroupPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div
+    v-if="isLoaded"
+  >
     <q-dialog v-model="showAddGroupDialog">
       <q-card>
         <q-card-section
@@ -12,6 +14,7 @@
               outlined
               dense
               type="number"
+              min=1
             />
           </div>
           <div class="q-pa-xs">
@@ -63,7 +66,7 @@
           />
           <q-btn
             flat
-            label="Hinzufügen"
+            :label="newGroup.id ? 'Speichern' : 'Hinzufügen'"
             @click="addGroup"
           />
         </q-card-actions>
@@ -137,13 +140,40 @@
         @update:model-value="getParticipators"
         style="min-width: 100px"
       />
-      <q-btn flat class="q-ml-md" :label="editable ? 'Bearbeiten beenden' : 'Bearbeiten'" @click="toggleEdit" />
-      <q-btn flat class="q-ml-md" label="Gruppe Hinzufügen" @click="openAddGroupDialog" />
+      <q-btn
+        flat
+        class="q-ml-md"
+        label="MA Bearbeiten"
+        @click="toggleUserEdit"
+        :color="editableUser ? 'primary' : ''"
+      />
+      <q-btn
+        flat
+        class="q-ml-md"
+        label="TN Bearbeiten"
+        @click="toggleParticipatorEdit"
+        :color="editableParticipator ? 'primary' : ''"
+      />
+      <q-btn
+        flat
+        class="q-ml-md"
+        label="Gruppe bearbeiten"
+        @click="toggleGroupEdit"
+        :color="editableGroup ? 'primary' : ''"
+      />
+      <q-btn
+        v-if="editableGroup"
+        flat
+        class="q-ml-md"
+        icon="fas fa-plus"
+        label="Gruppe hinzufügen"
+        @click="openAddGroupDialog()"
+      />
     </q-toolbar>
 
     <div class="q-pa-md row no-wrap">
       <div
-        v-show="group.groupNumber !== 0 || editable"
+        v-show="group.groupNumber !== 0 || editableParticipator || editableUser"
         v-for="group in groupList.filter(group => group.type === 0 || group.type === 1)"
         :key="group.id"
         class="column"
@@ -154,7 +184,18 @@
           style="min-height: 150px;"
         >
           <q-card-section>
-            <div class="text-subtitle1 row" style="justify-content: center; white-space: nowrap;">{{ (group.groupNumber ? 'Gruppe ' + group.groupNumber + ' - ' : '') + group.title }}</div>
+            <div class="text-subtitle1 row" style="justify-content: center; white-space: nowrap;">
+              {{ (group.groupNumber ? 'Gruppe ' + group.groupNumber + ' - ' : '') + group.title }}
+              <q-btn
+                v-if="editableGroup && group.id"
+                class="q-ml-md"
+                flat
+                dense
+                size="xs"
+                icon="fas fa-pen"
+                @click="openAddGroupDialog(group.id)"
+              />
+            </div>
           </q-card-section>
           <q-card-section
             v-if="group.id"
@@ -219,23 +260,25 @@
           />
           <q-card-section
             class="q-pa-none"
-            v-if="group.id"
+            v-if="group.id || editableUser"
           >
             <div class="q-pa-xs text-subtitle2" style="white-space: nowrap;">Mitarbeiter</div>
             <q-list
-              v-for="user in group.GroupUsers"
-              :key="user"
+              :id="'groupUser-list-' + group.id"
             >
               <q-item
+                v-for="groupUser in group.GroupUsers"
+                :key="groupUser"
                 class="q-pa-xs q-my-xs"
                 style="border: 1px solid grey; border-radius: 5px;"
-                :to="'/l/profile/' + user.uuid"
+                :to="'/l/profile/' + groupUser.uuid"
+                :id="'groupUser-item-' + groupUser.uuid"
               >
                 <q-item-section no-wrap>
                   <q-item-label>
-                    {{ user.User.firstName }} {{ user.User.lastName }}
+                    {{ users[groupUser.uuid].firstName }} {{ users[groupUser.uuid].lastName }}
                     <q-icon
-                      v-if="user.type === 2"
+                      v-if="groupUser.type === 2"
                       name="fas fa-bolt"
                       color="yellow"
                       title="GL"
@@ -243,34 +286,37 @@
                   </q-item-label>
                   <q-item-label caption>
                     <q-icon
-                      :name="'fas fa-' + (user.User.gender === 'w' ? 'venus' : 'mars')"
-                      :color="user.User.gender === 'w' ? 'pink' : 'blue'"
-                      :title="user.User.gender === 'w' ? 'weiblich' : 'männlich'"
+                      :name="'fas fa-' + (users[groupUser.uuid].gender === 'w' ? 'venus' : 'mars')"
+                      :color="users[groupUser.uuid].gender === 'w' ? 'pink' : 'blue'"
+                      :title="users[groupUser.uuid].gender === 'w' ? 'weiblich' : 'männlich'"
                     />
                     <q-icon
                       :name="'fas fa-guitar'"
-                      :color="getSkillColor(user.User.UserYears[0].guitar)"
-                      :title="$constants.engagement.roles.guitar.options.find(o => o.value === user.User.UserYears[0].guitar).label"
+                      :color="getSkillColor(users[groupUser.uuid].guitar)"
+                      :title="$constants.engagement.roles.guitar.options.find(o => o.value === users[groupUser.uuid].guitar).label"
                       class="q-pl-xs"
                     />
                   </q-item-label>
                 </q-item-section>
                 <q-item-section side no-wrap>
-                  <q-item-label :title="getFormattedBirthday(user.User)">
-                    {{ getAge(user.User) }}
+                  <q-item-label :title="getFormattedBirthday(users[groupUser.uuid])">
+                    {{ getAge(users[groupUser.uuid]) }}
                   </q-item-label>
                   <q-item-label caption
                     style="max-width: 150px; overflow: hidden; text-overflow: ellipsis;"
-                    :title="user.User.church"
+                    :title="users[groupUser.uuid].church"
                   >
-                    {{ user.User.church }}
+                    {{ users[groupUser.uuid].church }}
                   </q-item-label>
                 </q-item-section>
               </q-item>
             </q-list>
           </q-card-section>
-          <q-separator />
+          <q-separator
+            v-if="group.id"
+          />
           <q-card-section
+            v-if="group.id || editableParticipator"
             class="q-pa-none"
           >
             <div class="q-pa-xs text-subtitle2" style="white-space: nowrap;">Teilnehmer</div>
@@ -309,7 +355,7 @@
                   >
                     <q-item-label>
                       <q-btn
-                        v-if=" editable && !preference.Participators.length"
+                        v-if=" editableParticipator && !preference.Participators.length"
                         flat
                         dense
                         size="xs"
@@ -331,8 +377,8 @@
                       :id="'participator-item-' + getParticipatorId(participator)"
                       clickable
                       class="q-mt-xs"
-                      :to="editable ? null : '/l/leader/participator/' + participator.orderId + '/' + participator.positionId"
-                      @click="editable ? openShowWishDialog(participator, group) : null"
+                      :to="editableParticipator ? null : '/l/leader/participator/' + participator.orderId + '/' + participator.positionId"
+                      @click="editableParticipator ? openShowWishDialog(participator, group) : null"
                     >
                       <q-item-section
                         no-wrap
@@ -369,7 +415,7 @@
           </q-card-section>
         </q-card>
         <q-btn
-          v-if="editable"
+          v-if="editableParticipator"
           label="Block hinzufügen"
           flat
           dense
@@ -377,6 +423,15 @@
         />
       </div>
     </div>
+  </div>
+  <div
+    v-else
+    class="q-pa-md flex flex-center full-height"
+  >
+    <q-spinner-puff
+      color="primary"
+      size="50px"
+    />
   </div>
 </template>
 
@@ -392,13 +447,17 @@ export default defineComponent({
   setup() {
     const { proxy } = getCurrentInstance();
     const c = proxy.$constants
+    const isLoaded = ref(false);
     const showAddGroupDialog = ref(false);
     const showWishDialog = ref(false);
     const wishDialogModel = ref({});
     const settings = proxy.$settings;
-    const editable = ref(false);
+    const editableUser = ref(false);
+    const editableParticipator = ref(false);
+    const editableGroup = ref(false);
     const groupList = ref([]);
     const participators = ref({});
+    const users = ref({});
     const groupPreference = ref({});
     const newGroup = ref({});
     const sortables = ref([]);
@@ -413,19 +472,43 @@ export default defineComponent({
       { label: 'Infrastruktur', value: 2 },
     ]);
 
-    const toggleEdit = () => {
-      editable.value = !editable.value;
-      if (editable.value) {
+    const toggleEditMode = (mode) => {
+      if ((editableUser.value || editableParticipator.value) && 
+        (mode !== 'user' && mode !== 'participator' || 
+         !editableUser.value && !editableParticipator.value)) {
+      sortables.value.forEach(sortable => sortable.destroy());
+      sortables.value = [];
+      }
+      
+      editableUser.value = false;
+      editableParticipator.value = false;
+      editableGroup.value = false;
+      
+      if (mode === 'user') {
+        editableUser.value = true;
         resetSortables();
-      } else {
-        sortables.value.forEach(sortable => {
-          sortable.destroy();
-          sortables.value = [];
-        });
+      } else if (mode === 'participator') {
+        editableParticipator.value = true;
+        resetSortables();
+      } else if (mode === 'group') {
+        editableGroup.value = true;
       }
     };
 
+    const toggleUserEdit = () => {
+      editableUser.value ? toggleEditMode(null) : toggleEditMode('user');
+    };
+ 
+    const toggleParticipatorEdit = () => {
+      editableParticipator.value ? toggleEditMode(null) : toggleEditMode('participator');
+    };
+
+    const toggleGroupEdit = () => {
+      editableGroup.value ? toggleEditMode(null) : toggleEditMode('group');
+    };
+
     const resetSortables = () => {
+      if (editableParticipator.value) {
         groupList.value.forEach((group, index) => {
           const el = document.getElementById('groupPreference-list-' + group.id);
           if (!el) return;
@@ -467,17 +550,47 @@ export default defineComponent({
             }));
           });
         });
+      } else if (editableUser.value) {
+        groupList.value.forEach((group, index) => {
+          const el = document.getElementById('groupUser-list-' + group.id);
+          if (!el) return;
+          sortables.value.push(Sortable.create(el, {
+            group: 'user',
+            animation: 150,
+            onEnd: async (event) => {
+              console.log(event);
+              
+              const uuid = event.item.id.split('-').splice(2).join('-');
+              const fromId = +event.from.id.split('-')[2];
+              const toId = +event.to.id.split('-')[2];
+              if (fromId !== toId) {
+                if (fromId) {
+                  await api.delete('/groupUser/' + fromId + '/' + uuid)
+                }
+                if (toId) {
+                  await api.post('/groupUser/' + toId + '/' + uuid);
+                }
+                refreshGroups();
+              }
+            }
+          }));
+        });
+      }
     };
 
-    const openAddGroupDialog = () => {
-      newGroup.value = {
-        year: settings.currentYear,
-        week: week.value.value,
-        groupNumber: groupList.value.length,
-        title: "",
-        color: "",
-        type: typeOptions.value[0]
-      };
+    const openAddGroupDialog = (groupId) => {
+      if (groupId) {
+        newGroup.value = groupList.value.find(g => g.id === groupId);
+      } else {
+        newGroup.value = {
+          year: settings.currentYear,
+          week: week.value.value,
+          groupNumber: groupList.value.length,
+          title: "",
+          color: "",
+          type: typeOptions.value[0]
+        };
+      }
       showAddGroupDialog.value = true;
     };
 
@@ -506,24 +619,42 @@ export default defineComponent({
     };
 
     const refreshGroups = () => {
-      api.get('/group?year=' + settings.currentYear + '&week=' + week.value.value + '&participatorBundle&userBundle').then((response) => {
+      api.get('/group?year=' + settings.currentYear + '&week=' + week.value.value + '&participatorBundle&groupUserBundle').then((response) => {
         for (let p in participators.value) {
           participators.value[p].groupId = 0;
           participators.value[p].preferenceId = 0;
           participators.value[p].ignoredWishes = '';
+        }
+        for (let u in users.value) {
+          users.value[u].groupId = 0;
         }
         groupList.value = response.data;
         groupList.value.forEach((group, groupIndex) => {
           group.Preferences.forEach((preference, preferenceIndex) => {
             preference.Participators.forEach((participator, participatorIndex) => {
               const pId = getParticipatorId(participator);
-              console.log(pId);
               participators.value[pId].groupId = group.id;
               participators.value[pId].preferenceId = preference.id;
               participators.value[pId].ignoredWishes = participator.ignoredWishes;
               groupList.value[groupIndex].Preferences[preferenceIndex].Participators[participatorIndex] = participators.value[pId];
             })
           });
+          group.GroupUsers.forEach((groupUser, userIndex) => {
+            const uId = groupUser.uuid;
+            
+            users.value[uId].groupId = group.id;
+          });
+        });
+        let groupUsers = [];
+        Object.entries(users.value).forEach((entry) => {
+          const [index, item] = entry;
+          if (item.groupId === 0) {
+            groupUsers.push({
+              uuid: item.uuid,
+              type: 0,
+              groupId: 0
+            });
+          }
         });
         groupList.value.push({
           id: 0,
@@ -537,14 +668,17 @@ export default defineComponent({
             id: 0,
             groupId: 0,
             Participators: Object.values(participators.value).filter(p => !p.groupId)
-          }]
+          }],
+          GroupUsers: groupUsers
         });
         groupList.value = groupList.value.sort((a, b) => a.groupNumber - b.groupNumber);
+        isLoaded.value = true;
       });
     };
 
     const getParticipators = () => {
-      api
+      isLoaded.value = false;
+      const promise = api
         .get("/participator")
         .then(function (response) {
           participators.value = [];
@@ -554,17 +688,35 @@ export default defineComponent({
               participators.value[getParticipatorId(item)] = item;
             }
           });
-          refreshGroups();
         })
         .catch(function (e) {
 
         });
+      return promise;
     }
+
+    const getUsers = () => {
+      isLoaded.value = false;
+      const promise = api.get('/userYear?status=4&year=' + settings.currentYear + '&userBundle').then(function(response) {
+          users.value = [];
+          Object.entries(response.data).forEach((entry => {
+            const [index, item] = entry
+            let row = item
+            Object.entries(item.UserModel).forEach((entry => {
+              const [index, item] = entry
+              row[index] = item
+            }))
+            delete row.UserModel;
+            users.value[row.uuid] = row;
+          }))
+        }).catch(function(e) {})
+      return promise;
+    };
 
     const addGroup = () => {
       let values = newGroup.value;
       values.type = values.type.value;
-      api.post("/group", newGroup.value).then(refreshGroups);
+      api.post("/group/" + (values.id || ''), values).then(refreshGroups);
       closeAddGroupDialog();
     };
 
@@ -655,17 +807,16 @@ export default defineComponent({
     }
 
     const getUserInfoByGroup = (group) => {
-      const groupUsers = group.GroupUsers || [];
-      let users = [];
-      groupUsers.forEach((gu) => {
-        users.push(gu.User);
+      const groupUsers = [];
+      group.GroupUsers || [].forEach((gu) => {
+        u.push(users.value[gu.uuid]);
       });
-      const ages = users.map(u => +getAge(u));
+      const ages = groupUsers.map(u => +getAge(u));
       const avg = (ages.reduce((a, b) => a + b, 0) / ages.length).toFixed(2);
-      const m = users.filter(u => u.gender === 'm').length;
-      const f = users.filter(u => u.gender === 'w').length;
+      const m = groupUsers.filter(u => u.gender === 'm').length;
+      const f = groupUsers.filter(u => u.gender === 'w').length;
       return {
-        'count': users.length,
+        'count': groupUsers.length,
         'avg': avg,
         'm': m,
         'f': f
@@ -706,9 +857,14 @@ export default defineComponent({
       }).then(refreshGroups);
     }
 
-    getParticipators();
+    const participatorPromise = getParticipators();
+    const userPromise = getUsers();
+    Promise.all([participatorPromise, userPromise]).then(() => {
+      refreshGroups();
+    });
 
     return {
+      isLoaded,
       showAddGroupDialog,
       showWishDialog,
       wishDialogModel,
@@ -717,15 +873,20 @@ export default defineComponent({
       typeOptions,
       groupList,
       participators,
+      users,
       groupPreference,
       week,
-      editable,
+      editableUser,
+      editableParticipator,
+      editableGroup,
       openAddGroupDialog,
       closeAddGroupDialog,
       openShowWishDialog,
       closeWishDialog,
       addGroup,
-      toggleEdit,
+      toggleUserEdit,
+      toggleParticipatorEdit,
+      toggleGroupEdit,
       resetSortables,
       getAge,
       getFormattedBirthday,
@@ -742,7 +903,7 @@ export default defineComponent({
     };
   },
   updated() {
-    if (this.editable) {
+    if (this.editableParticipator || this.editableUser) {
       this.resetSortables();
     }
   }
